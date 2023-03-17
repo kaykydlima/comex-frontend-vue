ea86223e89ef3129a8badd2c99eea04fd5ae1f87
kayky <kayky.lima@onebox.one>
2023-03-17 11:05:35 -0300

    finalizando menu

--- src/components/barra-navegacao/BarraNavegacao.vue
+++ src/components/barra-navegacao/BarraNavegacao.vue
@@ -1,35 +1,47 @@
 <template>
-  <MenuMobile/>
+  <nav class="navbar navbar-expand-lg bg-dark navbar-dark p-4">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01"
+        aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse px-20" id="navbarTogglerDemo01">
+        <a class="navbar-brand" href="#"><img src="../../assets/logo.svg" alt="logo comex" class="img"></a>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Produtos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active">Categorias</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
 </template>
-
 <script lang="ts">
   import { defineComponent } from 'vue';
-  import MenuMobile from './MenuMobile.vue';
+
   export default defineComponent({
     name: 'BarraNavegacao',
-    components: {
-      MenuMobile,
-    }
+
   })
-
 </script>
-
-<style>
-  .menu {
-    align-items: center;
-    background: var(--roxo);
-    display: flex;
-    height: 50px;
-    justify-content: space-between;
+<style scoped>
+  .img {
+    width: 100px;
   }
 
-  .logo{
-      padding: 12px;
-  }
-
-  .cabecalho{
-      display: flex;
-      flex-direction: column; 
-      justify-content: center;
+  @media screen and (max-width: 991px) {
+    .img {
+      margin: 1rem 0;
+    }
   }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import '@/assets/base.css'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap'
 
 createApp(App).mount('#app')
